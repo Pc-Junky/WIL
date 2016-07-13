574aeb374220459731ac1ebee9b067b43af36c68
AdrianBeukes <AdrianBeukes@Gmail.com>
2016-07-13 09:26:09 +0200

    Add files via upload

---
+++ WIL-Login/WIL-Login/Login.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WIL_Login
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnForgotPass = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblGraphics = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblLogo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.BackColor = System.Drawing.Color.Transparent;
+            this.lblHeading.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblHeading.Location = new System.Drawing.Point(71, 22);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(355, 22);
+            this.lblHeading.TabIndex = 23;
+            this.lblHeading.Text = "Scan your ID card or enter your login details below";
+            this.lblHeading.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRegister.Location = new System.Drawing.Point(141, 185);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(199, 35);
+            this.btnRegister.TabIndex = 22;
+            this.btnRegister.Text = "Register User";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnForgotPass
+            // 
+            this.btnForgotPass.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnForgotPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForgotPass.Location = new System.Drawing.Point(141, 132);
+            this.btnForgotPass.Name = "btnForgotPass";
+            this.btnForgotPass.Size = new System.Drawing.Size(92, 40);
+            this.btnForgotPass.TabIndex = 21;
+            this.btnForgotPass.Text = "  Forgot \r\nPassword";
+            this.btnForgotPass.UseVisualStyleBackColor = false;
+            this.btnForgotPass.Click += new System.EventHandler(this.btnForgotPass_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(141, 96);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(199, 20);
+            this.txtPassword.TabIndex = 20;
+            this.txtPassword.Text = "Password";
+            // 
+            // lblGraphics
+            // 
+            this.lblGraphics.AutoSize = true;
+            this.lblGraphics.BackColor = System.Drawing.Color.Transparent;
+            this.lblGraphics.Location = new System.Drawing.Point(12, 66);
+            this.lblGraphics.Name = "lblGraphics";
+            this.lblGraphics.Size = new System.Drawing.Size(113, 13);
+            this.lblGraphics.TabIndex = 19;
+            this.lblGraphics.Text = "Graphics inserted here";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSubmit.Location = new System.Drawing.Point(248, 132);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(92, 40);
+            this.btnSubmit.TabIndex = 18;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(141, 63);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(199, 20);
+            this.txtName.TabIndex = 17;
+            this.txtName.Text = "Username";
+            // 
+            // lblLogo
+            // 
+            this.lblLogo.AutoSize = true;
+            this.lblLogo.BackColor = System.Drawing.Color.Transparent;
+            this.lblLogo.Location = new System.Drawing.Point(371, 63);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(95, 13);
+            this.lblLogo.TabIndex = 16;
+            this.lblLogo.Text = "Logo inserted here";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 232);
+            this.Controls.Add(this.lblHeading);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnForgotPass);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblGraphics);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblLogo);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "                                                                 Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnForgotPass;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblGraphics;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblLogo;
+    }
+}
+
---
+++ WIL-Login/WIL-Login/Login.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WIL_Login
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        //**********************************************************************************************************************************
+        private void btnRegister_Click(object sender, EventArgs e)              //Register user button
+        {
+            this.Hide();                                                        //Hides current form
+            //RegisterFormName RegisterForm = new RegisterFormName();           //Create a new instance of the Form class
+
+            //RegisterForm.Show();                                              //Will open Register form
+        }
+        //**********************************************************************************************************************************
+        private void btnSubmit_Click(object sender, EventArgs e)                //Submit button
+        {
+            bool Valid = false;
+
+            //not tested - reading from the database
+            //
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //using (SqlCommand command = new SqlCommand("select * from databaseName where Complete = 0", connection))
+            //{
+            //    connection.Open();
+            //    using (SqlDataReader reader = command.ExecuteReader())
+            //    {
+            //        while (reader.Read())
+            //        {
+            //            string Name = reader.GetString(0);
+            //            string Password = reader.GetString(1);
+            //  
+            //            if((Name == txtName.Text && Password == txtPassword.Text)
+            //            {
+            //              Valid = true;      
+            //              break;
+            //            }
+            //        }
+            //    }
+            //}
+
+            if (Valid)
+            {
+                MessageBox.Show("Login succesfull");                             //message login worked
+
+                this.Hide();                                                     //hides current form
+
+                //if we add splash screen 
+                //
+                //Creating thread for BeginSplash Method so it can start and stop   //when successfully logged in, it will start the splash screen
+                //Thread thrd = new Thread(new ThreadStart(BeginSplash));
+                //thrd.Start();
+                //Thread.Sleep(8500);
+
+                //thrd.Abort();                                                    //when timer stops, so will the splash screen
+
+                //HomeFormName HomeForm = new HomeFormName();                    // Create a new instance of the Form class
+
+                //HomeForm.Show();                                             // Shows the Home form
+            }
+            else
+            {
+                MessageBox.Show("Login failed /n Please make sure you entered the correct credentials, and try again");   //message if login failed
+            }
+        }
+        //**********************************************************************************************************************************
+        private void btnForgotPass_Click(object sender, EventArgs e)
+        {
+            //new form allowing user to enter his email, and position
+            //checks to see if email and position matches
+            //then randomize a code
+            //send towards his email
+            //let him enter matching code
+            //if matches allows for editing his password
+        }
+        //**********************************************************************************************************************************
+    }
+}
